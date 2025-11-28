a2d75200f0a8b625bdc8d73ce91230126c71ed11
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2025-11-28 10:16:19 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sf-09",
+      "name": "SF-09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.17610941321754225,
+        "pitch": 0.16271068476431338,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SF-09",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
